1b35f3cf56e396d25f402e914a101667b79b2c9f
FJVIKTORIJA <83015268+FJVIKTORIJA@users.noreply.github.com>
2021-08-17 10:13:43 +0100

    Fixed code/Generating password

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 
-var characteramountrange = document.getElementById ('characteramountrange')
-var characteramountnumber = document.getElementById ('characteramountnumber')
-var IncludeuppercaseElement = document.getElementById ('Includeuppercase')
+var generateBtn = document.querySelector("#generate");
+var characteramountrange = document.getElementById('characteramountrange')
+var characteramountnumber = document.getElementById('characteramoutnumber')
+var IncludeuppercaseElement = document.getElementById('Includeuppercase')
 
-var Includenumber= document.getElementById ('Includenumber')
-var Includespecialcharackter= document.getElementById ('Includespecialcharacter')
-var form = document.getElementById ('passwordgeneratorform')
-var passwordDisplay = document.getElementById ('passwordDisplay')
+var Includenumber = document.getElementById('Includenumber')
+var Includespecialcharacter = document.getElementById('Includespecialcharacter')
+var form = document.getElementById('passwordgeneratorform')
+var passwordDisplay = document.getElementById('passwordDisplay')
 
 var UPPERCASE_CHAR_CODES = arrayFromLowToHight(65, 90)
 var LOWERCASE_CHAR_CODES = arrayFromLowToHight(97, 122)
@@ -19,48 +20,50 @@ var SPECIALCHARACTER_CHAR_CODES = arrayFromLowToHight(33, 47).concat(
   arrayFromLowToHight(123, 126)
 )
 
-charcteramountnumber.addEventListener('input', syncharacterAmount)
-charcteramountrange.addEventListener('input', syncharacterAmount)
+characteramountnumber.addEventListener('input', syncharacterAmount);
+// charcteramountrange.addEventListener('input', syncCharacterAmount);
 
-form.addEventListener('submit', e => { 
-  e.preventDefault()
-  const characteramount = characteramountnumber.value
-  const includeuppercase = IncludeuppercaseElement.checked
-  const includenumber = IncludenumberElement.checked
-  const includespecialcharacter = IncludespecialcharacterElement.checked
-  const password = generatePassword(characterAmount, Includeuppercase, Includelowercase, Includenumber, Includespecialcharacter)
-  passwordDisplay.innerText = password 
-   })
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  console.log(characteramountnumber);
+  const characteramount = characteramountnumber.value;
+  const includeuppercase = IncludeuppercaseElement.checked;
+  const includenumber = Includenumber.checked;
+  const includespecialcharacter = Includespecialcharacter.checked;
+  console.log(includespecialcharacter, includenumber, includeuppercase, characteramount);
+  const password = generatePassword(characteramount, includeuppercase, includenumber, includespecialcharacter);
+  passwordDisplay.innerText = password;
+});
 
-function generatePassword (characteramount, includeuppercase, includenumber, includespecialcharacter) { 
+function generatePassword(characteramount, includeuppercase, includenumber, includespecialcharacter) {
   let charCodes = LOWERCASE_CHAR_CODES
-  if (includeuppercase) charCodes = charCodes.concat (UPPERCASE_CHAR_CODES)
-  if (includenumber) charCodes = charCodes.concat (NUMBER_CHAR_CODES)
-  if (includespecialcharacter) charCodes = charCodes.concat (SPECIALCHARACTER_CHAR_CODES)
+  if (includeuppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
+  if (includenumber) charCodes = charCodes.concat(NUMBER_CHAR_CODES)
+  if (includespecialcharacter) charCodes = charCodes.concat(SPECIALCHARACTER_CHAR_CODES)
 
-  const passwordcharackters = []
+  const passwordcharacters = []
   for (let i = 0; i < characteramount; i++) {
     const characterCode = charCodes[Math.floor(Math.random() * characteramount)]
     passwordcharacters.push(String.fromCharCode(characterCode))
 
   }
-  return passwordcharackters.join ('')
+  return passwordcharacters.join('')
 }
 
 function arrayFromLowToHight(low, high) {
   const array = []
-  for (let i= low; i <=high; i++) {
+  for (let i = low; i <= high; i++) {
     array.push(i)
- }
- return array
+  }
+  return array
 }
 
-function syncCharacterAmount(e) {
-  var value = e.target.value 
-  characteramountnumber.value = value
-  characteramountrange.value = value
-
+function syncharacterAmount(e) {
+  var value = e.target.value;
+  characteramountnumber.value = value;
+  
 }
+
 
 
 
