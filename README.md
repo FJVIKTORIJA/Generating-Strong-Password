b4a453dbd357456cd74e7e8861f7f91ccb3c75f4
FJVIKTORIJA <83015268+FJVIKTORIJA@users.noreply.github.com>
2021-08-25 23:42:16 +0100

    Read me update.

--- README.md
+++ README.md
@@ -4,6 +4,10 @@
 
 This application allows to randomize a password. It generates a strong password between 8 and 129 characters with an option to include uppercase, number and special characterer.
 
+<img width="1212" alt="Screenshot 2021-08-25 at 23 35 58" src="https://user-images.githubusercontent.com/83015268/130873485-541bd2a7-a0ac-4d9e-8eb1-9b61cae1b566.png">
+
 # Usage
 
 To use this application it is needed to select a number of characters that wish a password to have and select which type of characters to include. Once the button 'Generate password' os clicked the pasword will be generated and displayed in the section 'Your Secure Password'.
+
+[Link to Application](https://fjviktorija.github.io/Generating-Strong-Password/)
--- script.js
+++ script.js
@@ -1,73 +1,78 @@
-
 var generateBtn = document.querySelector("#generate");
-var characteramountrange = document.getElementById('characteramountrange')
-var characteramountnumber = document.getElementById('characteramoutnumber')
-var IncludeuppercaseElement = document.getElementById('Includeuppercase')
+var characteramountrange = document.getElementById("characteramountrange");
+var characteramountnumber = document.getElementById("characteramoutnumber");
+var IncludeuppercaseElement = document.getElementById("Includeuppercase");
 
-var Includenumber = document.getElementById('Includenumber')
-var Includespecialcharacter = document.getElementById('Includespecialcharacter')
-var form = document.getElementById('passwordgeneratorform')
-var passwordDisplay = document.getElementById('passwordDisplay')
+var Includenumber = document.getElementById("Includenumber");
+var Includespecialcharacter = document.getElementById(
+  "Includespecialcharacter"
+);
+var form = document.getElementById("passwordgeneratorform");
+var passwordDisplay = document.getElementById("passwordDisplay");
 
-var UPPERCASE_CHAR_CODES = arrayFromLowToHight(65, 90)
-var LOWERCASE_CHAR_CODES = arrayFromLowToHight(97, 122)
-var NUMBER_CHAR_CODES = arrayFromLowToHight(48, 57)
-var SPECIALCHARACTER_CHAR_CODES = arrayFromLowToHight(33, 47).concat(
-  arrayFromLowToHight(58, 64)
-).concat(
-  arrayFromLowToHight(91, 96)
-).concat(
-  arrayFromLowToHight(123, 126)
-)
+var UPPERCASE_CHAR_CODES = arrayFromLowToHight(65, 90);
+var LOWERCASE_CHAR_CODES = arrayFromLowToHight(97, 122);
+var NUMBER_CHAR_CODES = arrayFromLowToHight(48, 57);
+var SPECIALCHARACTER_CHAR_CODES = arrayFromLowToHight(33, 47)
+  .concat(arrayFromLowToHight(58, 64))
+  .concat(arrayFromLowToHight(91, 96))
+  .concat(arrayFromLowToHight(123, 126));
 
-characteramountnumber.addEventListener('input', syncharacterAmount);
+characteramountnumber.addEventListener("input", syncharacterAmount);
 // charcteramountrange.addEventListener('input', syncCharacterAmount);
 
-form.addEventListener('submit', e => {
+form.addEventListener("submit", (e) => {
   e.preventDefault();
   console.log(characteramountnumber);
   const characteramount = characteramountnumber.value;
   const includeuppercase = IncludeuppercaseElement.checked;
   const includenumber = Includenumber.checked;
   const includespecialcharacter = Includespecialcharacter.checked;
-  console.log(includespecialcharacter, includenumber, includeuppercase, characteramount);
-  const password = generatePassword(characteramount, includeuppercase, includenumber, includespecialcharacter);
+  console.log(
+    includespecialcharacter,
+    includenumber,
+    includeuppercase,
+    characteramount
+  );
+  const password = generatePassword(
+    characteramount,
+    includeuppercase,
+    includenumber,
+    includespecialcharacter
+  );
   passwordDisplay.innerText = password;
 });
 
-function generatePassword(characteramount, includeuppercase, includenumber, includespecialcharacter) {
-  let charCodes = LOWERCASE_CHAR_CODES
-  if (includeuppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
-  if (includenumber) charCodes = charCodes.concat(NUMBER_CHAR_CODES)
-  if (includespecialcharacter) charCodes = charCodes.concat(SPECIALCHARACTER_CHAR_CODES)
+function generatePassword(
+  characteramount,
+  includeuppercase,
+  includenumber,
+  includespecialcharacter
+) {
+  let charCodes = LOWERCASE_CHAR_CODES;
+  if (includeuppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES);
+  if (includenumber) charCodes = charCodes.concat(NUMBER_CHAR_CODES);
+  if (includespecialcharacter)
+    charCodes = charCodes.concat(SPECIALCHARACTER_CHAR_CODES);
 
-  const passwordcharacters = []
+  const passwordcharacters = [];
   for (let i = 0; i < characteramount; i++) {
-    const characterCode = charCodes[Math.floor(Math.random() * characteramount)]
-    passwordcharacters.push(String.fromCharCode(characterCode))
-
+    const characterCode =
+      charCodes[Math.floor(Math.random() * characteramount)];
+    passwordcharacters.push(String.fromCharCode(characterCode));
   }
-  return passwordcharacters.join('')
+  return passwordcharacters.join("");
 }
 
 function arrayFromLowToHight(low, high) {
-  const array = []
+  const array = [];
   for (let i = low; i <= high; i++) {
-    array.push(i)
+    array.push(i);
   }
-  return array
+  return array;
 }
 
 function syncharacterAmount(e) {
   var value = e.target.value;
   characteramountnumber.value = value;
-  
 }
-
-
-
-
-
-
-
-
